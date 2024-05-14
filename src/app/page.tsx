580bd6199ecab2d53a828fb2c19fef5becfeb54c
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-05-14 15:31:50 +0530

    style: Add TailwindCSS prettier plugin for consistent formatting of TailwindCSS utility classes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className={"min-h-screen flex"}>
+    <main className={"flex min-h-screen"}>
       <div className={"m-auto"}>
-        <div className={"flex flex-col justify-center items-center space-y-3"}>
+        <div className={"flex flex-col items-center justify-center space-y-3"}>
           <h1 className={"text-2xl font-bold"}>Food Yatri</h1>
           <p className={"text-lg"}>
             Empowering food vendors by connoting to potential customers.
